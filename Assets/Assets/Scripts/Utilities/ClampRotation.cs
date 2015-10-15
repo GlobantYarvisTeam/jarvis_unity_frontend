a125132aaa784c481482db6be2198144abf39e04
andrescalandriglobant <andres.calandri@globant.com>
2015-10-15 17:43:43 -0300

    Added the idle animation for the rings

---
+++ Assets/Assets/Scripts/Utilities/ClampRotation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClampRotation : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.rotation = Quaternion.Euler(Vector3.zero);
+	}
+}
