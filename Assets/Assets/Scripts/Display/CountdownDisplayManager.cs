0ab9ade0883ad237c68b239ca222d2f9322f6c37
andrescalandriglobant <andres.calandri@globant.com>
2015-11-13 17:28:17 -0300

    Countdown will show days left in case its more than 1
    Now the countdown display will show how many days are left in
    case the time remaining is more than 24 hours.

--- Assets/Assets/Scripts/Display/CountdownDisplayManager.cs
+++ Assets/Assets/Scripts/Display/CountdownDisplayManager.cs
@@ -15,6 +15,14 @@ public class CountdownDisplayManager : IDisplayManager {
 	public Text textHours;
 	public Text textMins;
 	public Text textSecs;
+	public Text textHoursTitle;
+	public Text textMinsTitle;
+	public Text textSecsTitle;
+	public string daysTitle;
+	public string dayTitle;
+	public string hoursTitle;
+	public string minutesTitle;
+	public string secondsTitle;
 
 	public override void InitializeDisplay (int displayId)
 	{
@@ -24,7 +32,8 @@ public class CountdownDisplayManager : IDisplayManager {
 		userImageContainer.texture = Preloader.instance.GetImage (Preloader.instance.GetRunningDisplay(), "image");
 
 
-		conferenceTime = DateTime.Parse (Preloader.instance.GetString (Preloader.instance.GetRunningDisplay(), "time"));
+		conferenceTime = DateTime.UtcNow;//DateTime.Parse (Preloader.instance.GetString (Preloader.instance.GetRunningDisplay(), "time"));
+		conferenceTime = conferenceTime.AddDays (1);
 		//Debug.Log (Preloader.instance.GetString (Preloader.instance.GetRunningDisplay(), "time"));
 		//Debug.Log (conferenceTime);
 		//Debug.Log (DateTime.UtcNow);
@@ -40,14 +49,42 @@ public class CountdownDisplayManager : IDisplayManager {
 	void Update () {
 		TimeSpan timeUntilConference = conferenceTime - DateTime.UtcNow;
 
-		// The -1 is a hack to be revised since for some reason time is coming with an extra hour from the backend
-		string hours = (Mathf.FloorToInt ((float)timeUntilConference.TotalHours)).ToString ();
-		string minutes = timeUntilConference.Minutes.ToString ();
-		string seconds = timeUntilConference.Seconds.ToString ();
-		if(int.Parse(hours) < 0)
-		{
-			hours = minutes = seconds = "00";
+		int days = timeUntilConference.Days;
+		Debug.Log (days);
+		string hours = "0";
+		string minutes = "0";
+		string seconds = "0";
+
+		if (timeUntilConference.Days > 0) {
+			if(timeUntilConference.Days > 1)
+			{
+				textHoursTitle.text = daysTitle;
+			}
+			else
+			{
+				textHoursTitle.text = dayTitle;
+			}
+
+			textMinsTitle.text = hoursTitle;
+			textSecsTitle.text = minutesTitle;
+
+			hours = timeUntilConference.Days.ToString ();
+			minutes = timeUntilConference.Hours.ToString ();
+			seconds = timeUntilConference.Minutes.ToString ();
+		} else {
+			textHoursTitle.text = hoursTitle;
+			textMinsTitle.text = minutesTitle;
+			textSecsTitle.text = secondsTitle;
+			// The -1 is a hack to be revised since for some reason time is coming with an extra hour from the backend
+			hours = (Mathf.FloorToInt ((float)timeUntilConference.TotalHours)).ToString ();
+			minutes = timeUntilConference.Minutes.ToString ();
+			seconds = timeUntilConference.Seconds.ToString ();
+			if(int.Parse(hours) < 0)
+			{
+				hours = minutes = seconds = "00";
+			}
 		}
+
 		textHours.text = hours.Length == 1 ? "0" + hours : hours;
 		textMins.text = minutes.Length == 1 ? "0" + minutes : minutes;
 		textSecs.text = seconds.Length == 1 ? "0" + seconds : seconds;
